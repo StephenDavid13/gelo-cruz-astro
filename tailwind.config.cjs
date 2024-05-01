394e54abf53513d93896197c4144c3ad86f2dd41
Stephen David <stephen.david.06@gmail.com>
2024-05-01 21:24:50 +1000

    Added discography, profile pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				'brand-black': '#101112',
+			}
+		},
+		fontFamily: {
+			'sans': 'Inter',
+			'serif': 'PT Serif',
+			'mono': 'IMB Plex Mono',
+
+		}
 	},
 	plugins: [],
 }
